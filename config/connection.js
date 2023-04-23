ace174b99213291fca7037324168bdfb27bc5c9d
Jacob Goreham <jacobgoreham@gmail.com>
2023-04-22 21:48:30 -0700

    Fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require("mongoose");
 
-connect('mongodb://127.0.0.1:27017/developersApplications');
+connect("mongodb://127.0.0.1:27017/jacobs-social-network"),
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
 
 module.exports = connection;
